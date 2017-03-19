d486b0103ae21ba1b11449b58fad49510e028b79
MrJamesFeng <812463918@qq.co>
2017-03-19 11:14:46 +0800

    字体下载

--- 动态下载字体/动态下载字体/ViewController.m
+++ 动态下载字体/动态下载字体/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <CoreText/CoreText.h>
 @interface ViewController ()
 
 @end
@@ -17,10 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"检查字体" style:UIBarButtonItemStylePlain target:self action:@selector(checkDownFont)];
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
+-(void)checkDownFont{
+    NSString *fontName = @"DFWaWaSC-W5";//娃娃体
+    UIFont *font = [UIFont fontWithName:fontName size:14];
+    if ([font.fontName compare:fontName] == NSOrderedSame||[font.familyName compare:fontName] == NSOrderedSame) {
+        NSLog(@"字体存在");
+    }else{
+        NSLog(@"字体不存在");
+    }
+    NSMutableDictionary *attrs = [NSMutableDictionary dictionaryWithObjectsAndKeys:fontName,kCTFontNameAttribute, nil];
+    CTFontDescriptorRef desc = CTFontDescriptorCreateWithAttributes((__bridge CFDictionaryRef)attrs);
+    NSMutableArray *descs = [NSMutableArray array];
+    [descs addObject: (__bridge id)desc];
+    CFRelease(desc);
+    __block BOOL errorDuringDownload = NO;
+    CTFontDescriptorMatchFontDescriptorsWithProgressHandler((__bridge CFArrayRef)descs, NULL, ^bool(CTFontDescriptorMatchingState state, CFDictionaryRef  _Nonnull progressParameter) {
+        double progress = [[(__bridge NSDictionary *)progressParameter objectForKey:(id)kCTFontDescriptorMatchingPercentage] doubleValue] ;
+        if (state == kCTFontDescriptorMatchingDidBegin) {
+            NSLog(@"开始");
+        }
+        if (state ==  kCTFontDescriptorMatchingDidFinish) {
+            NSLog(@"完成");
+        }
+        if (state ==  kCTFontDescriptorMatchingDidFailWithError) {
+            NSLog(@"出错");
+        }
+        NSLog(@"progress=%f",progress);
+    
+        
+        return  YES;
+    });
+}- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
