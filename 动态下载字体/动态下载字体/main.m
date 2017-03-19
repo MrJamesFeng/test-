324dac80e7110d07fc22cfe039e443b080e27e20
MrJamesFeng <812463918@qq.co>
2017-03-19 10:09:47 +0800

    inital

---
+++ 动态下载字体/动态下载字体/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  动态下载字体
+//
+//  Created by LDY on 17/3/19.
+//  Copyright © 2017年 LDY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
